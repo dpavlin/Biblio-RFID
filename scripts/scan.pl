148d0dd6989f24ebc2964ed0a92f4c311890057a
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-07-27 13:46:56 +0200

    added --loop and --only 3M810

--- scripts/scan.pl
+++ scripts/scan.pl
@@ -4,18 +4,32 @@ use warnings;
 use strict;
 
 use Data::Dump qw(dump);
+use Getopt::Long;
+
+my $loop = 0;
+my $only;
+
+GetOptions(
+	'loop!'   => \$loop,
+	'only=s', => \$only,
+) || die $!;
+
+my @readers = ( '3M810', 'CPRM02' );
 
 use lib 'lib';
 
-foreach my $reader ( '3M810', 'CPRM02' ) {
-	my $module = "RFID::Serial::$reader";
-	eval "use $module";
-	die $@ if $@;
-	my $rfid = $module->new( device => '/dev/ttyUSB0' );
-	my $visible = $rfid->scan;
-	foreach my $tag ( keys %$visible ) {
-	warn "XXX $tag";
-		print "$tag\t", join('', @{ $visible->{$tag} }), $/;
+do {
+	foreach my $reader ( '3M810', 'CPRM02' ) {
+		next if $only && $only ne $reader;
+		my $module = "RFID::Serial::$reader";
+		eval "use $module";
+		die $@ if $@;
+		my $rfid = $module->new( device => '/dev/ttyUSB0' );
+		my $visible = $rfid->scan;
+		foreach my $tag ( keys %$visible ) {
+		warn "XXX $tag";
+			print "$reader\t$tag\t", join('', @{ $visible->{$tag} }), $/;
+		}
 	}
-}
 
+} while $loop;
